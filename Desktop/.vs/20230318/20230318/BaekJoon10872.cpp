d1e5ce8570ad4c089c80d14add847a30bd871bd9
nacfson <nacfson@gmail.com>
2023-03-19 15:00:09 +0900

    ee

--- Desktop/.vs/20230318/20230318/BaekJoon10872.cpp
+++ Desktop/.vs/20230318/20230318/BaekJoon10872.cpp
@@ -1,21 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int Factorial(int n)
-{
-	if (n < 1)
-	{
-		return 1;
-	
-	
-	}
-	return Factorial(n - 1);
-}
 
 int main(void)
 {
-	int n;
-
-	cin >> n;
+	cout << "Hello WOrld!";
 	return 0;
 }
