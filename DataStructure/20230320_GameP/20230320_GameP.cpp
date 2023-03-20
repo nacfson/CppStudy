dd202c8d99b759b83cb84ed362e4936132197cdc
nacfson <nacfson@gmail.com>
2023-03-20 11:01:53 +0900

    GameProgramming

---
+++ DataStructure/20230320_GameP/20230320_GameP.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+//getch(),kbhit();
+
+int main(void)
+{
+    long long curtTime = time(NULL);
+    while (1)
+    {
+        if (_kbhit)
+        {
+            cout << "끝!" << endl;
+            break;
+        }
+
+    }
+    if (time(NULL) - curtTime == 10)
+    {
+        cout << "정답";
+    }
+    else if (time(NULL) - curtTime < 10)
+    {
+        cout << " 빠름";
+    }
+    else
+    {
+        cout << "느림";
+    }
+    return 0;
+}
+
