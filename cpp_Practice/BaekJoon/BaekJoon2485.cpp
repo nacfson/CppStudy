2fee4fd5b9e1e6422942453a08708ff132b9f3aa
Yoo Hyung Joo <65493803+nacfson@users.noreply.github.com>
2023-07-23 22:38:37 +0900

    Baekjoon

---
+++ cpp_Practice/BaekJoon/BaekJoon2485.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(void){
+	int n;
+	cin >> n;
+
+	int arr[1000000000];
+
+	int minVal = 0;
+	int maxCnt = 0;
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < n; i++) {
+		int compareVal = arr[i + 1] - arr[i];
+		if (compareVal < minVal) compareVal = minVal;
+	}
+
+	for (int i = 0; i < n; i++) {
+		int compareVal = arr[i + 1] - arr[i];
+		if (compareVal == minVal) continue;
+		maxCnt += static_cast<int>(compareVal / minVal);
+	}
+
+	cout << maxCnt;
+	return 0;
+}
