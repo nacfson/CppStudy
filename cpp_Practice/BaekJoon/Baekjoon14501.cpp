ecb613d9625034324d03b6cefe64142ed29ec8c0
nacfson <nacfson@gmail.com>
2023-08-22 23:19:49 +0900

    CPp

--- cpp_Practice/BaekJoon/Baekjoon14501.cpp
+++ cpp_Practice/BaekJoon/Baekjoon14501.cpp
@@ -17,11 +17,8 @@ public:
 };
 
 int main(void) {
-	//시간 대비 최고 효율을 뽑는 사람들을 모은다.
-	//그 중 시간에 알맞게 딱 맞춘다. (이게 좀 어려울 듯)
-	//전체 탐색 vs 가중치 알고리즘
-
 	int n;
+
 	cin >> n;
 	
 	KVP kvpArr[15]{};
@@ -32,16 +29,12 @@ int main(void) {
 		KVP vp(day, profit);
 		kvpArr[i] = vp;
 	}
+	int curProfit = 0;
+	int curDay = 0;
 
-	for (int i = 0; i < n; i++) {
-		for (int j = i; j < n - 1; j++) {
-			if (kvpArr[j].GetWeight() > kvpArr[j+1].GetWeight()) {
-				KVP tmp = kvpArr[j];
-				kvpArr[j] = kvpArr[j+1];
-				kvpArr[j+1] = tmp;
-			}
-		}
-	}
+	int maxProfit = 0;
 	
 
+
+	cout << maxProfit;
 }
