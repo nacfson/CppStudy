777e23c0e319b770aa790298c452b916e54deaf4
Yoo Hyung Joo <65493803+nacfson@users.noreply.github.com>
2023-08-27 16:12:10 +0900

    qwe

--- cpp_Practice/BaekJoon/Baekjoon14889.cpp
+++ cpp_Practice/BaekJoon/Baekjoon14889.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <math.h>
+#include <map>
 using namespace std;
 int GetStats(int x,int y,int arr[20][20]) {
 	if (arr[x][y] == 0 || arr[y][x] == 0) return 0;
@@ -13,7 +14,9 @@ int main(void) {
 	cin >> n;
 
 	int linkArr[20][20];
-	vector<int> memo;
+	//vector<int> memo;
+
+	map<int, int> memo;
 	int result = 0;
 	
 	for (int i = 0; i < n; i++) {
@@ -29,7 +32,12 @@ int main(void) {
 			int compareVal = GetStats(i, j, linkArr);
 			if (compareVal == 0) continue;
 
-			memo.push_back(compareVal);
+			if (memo.find(compareVal) != memo.end()) {
+				memo[compareVal] += 1;
+			}
+			else {
+				memo.insert(std::make_pair(compareVal, 1));
+			}
 		}
 	}
 
@@ -39,17 +47,25 @@ int main(void) {
 			if (statVal == 0) continue;
 
 			int closestVal = 99999;
-			for (int i = 0; i < memo.size(); i++) {
-				int compareVal = abs(statVal - memo[i]);
+			for (auto it = memo.begin(); it != memo.end();) {
+				int compareVal = abs(statVal - it->first);
 
-				if (compareVal >= 0  && compareVal <  closestVal) {
+				if (compareVal >= 0 && compareVal < closestVal) {
 					closestVal = compareVal;
-					//memo.erase(0, memo.at(i));
+
+					if (it->second >= 1) {
+						memo[it->first] -= 1;
+					}
+					else {
+						it = memo.erase(it);
+					}
 					//Vector에서 제거하는 것도 실행시켜주어야 함
-					cout << "ClosestVal: " << closestVal;
+					///cout << "ClosestVal: " << closestVal;
+				}
+				else {
+					it++;
 				}
 			}
-
 			result += closestVal;
 		}
 	}
