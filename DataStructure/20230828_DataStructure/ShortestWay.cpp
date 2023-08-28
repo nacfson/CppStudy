6ee76efdd31f992c834e9078ce71bc92965e71bf
nacfson <nacfson@gmail.com>
2023-08-28 12:56:03 +0900

    cpp

---
+++ DataStructure/20230828_DataStructure/ShortestWay.cpp
@@ -0,0 +1,128 @@
+#define MAX_VTXS 200
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class AdjMatGraph
+{
+protected:
+	int size;
+	char vertices[MAX_VTXS];
+	int adj[MAX_VTXS][MAX_VTXS];
+
+	int visited[50];
+public:
+	AdjMatGraph() { reset(); }
+
+	char getVertex(int i) { return vertices[i]; }
+	int getEdge(int i, int j) {
+		return adj[i][j];
+	}
+	void setEdge(int i, int j, int val) {
+		adj[i][j] = val;
+	}
+	bool isEmpty() { return size == 0; }
+	bool isFull() { return size >= MAX_VTXS; }
+	void reset() {
+		size = 0;
+		for (int i = 0; i < MAX_VTXS; i++)
+			for (int j = 0; j < MAX_VTXS; j++)
+				setEdge(i, j, 0);
+	}
+
+	void insertVertex(char name) {
+		if (!isFull()) {
+			vertices[size++] = name;
+		}
+	}
+
+	void insertEdge(int t1, int t2) {
+		setEdge(t1, t2, 1);
+		setEdge(t2, t1, 1);
+	}
+	void display() {
+		cout << "  ";
+		for (int i = 0; i < size; i++) {
+			cout << vertices[i] << ' ';
+		}
+		cout << endl;
+		for (int i = 0; i < size; i++) {
+			cout << getVertex(i) << ' ';
+			for (int j = 0; j < size + 1; j++) {
+				cout << getEdge(i, j - 1) << ' ';
+			}
+			cout << '\n';
+		}
+	}
+	void resetVisited() {
+		for (int i = 0; i < size; i++) {
+			visited[i] = false;
+		}
+	}
+
+	bool isLinked(int u, int v) {
+		return getEdge(u, v) != 0;
+	}
+
+	void DFS(int n) {
+		cout << n << ' ';
+		visited[n] = true;
+
+		for (int i = 0; i < size; i++) {
+			if (isLinked(n, i) && visited[i] == false) {
+				DFS(i);
+			}
+		}
+	}
+
+	void BFS(int n) {
+		fill_n(visited,50, -1);
+
+		queue<int> q;
+		q.push(n);
+		//자기 자신이기때문에 0값을 넣어줌
+		visited[n] = 0;
+
+		while (!q.empty()) {
+			int a = q.front();
+			q.pop();
+
+			for (int i = 0; i < size; i++) {
+				if (isLinked(a, i) && visited[i] < 0) {
+					q.push(i);
+					visited[i] = visited[a] + 1;
+					cout << i;
+				}
+			}
+		}
+	}
+};
+
+
+int main() {
+	int n, m, temp1, temp2;
+
+
+
+	cout << "정점의 개수와 간선의 수 :";
+	cin >> n >> m;
+	cout << "간선의 시작정점과 끝 정점 :" << endl;
+
+	AdjMatGraph g;
+
+
+	for (int i = 0; i < n; i++)
+		g.insertVertex('A' + i);
+	for (int i = 0; i < m; i++)
+	{
+		cin >> temp1 >> temp2;
+		g.insertEdge(temp1,
+			temp2);
+	}
+	cout << "인접 행렬로 표현한 그래프" << endl;
+	g.display();
+	cout << "DFS 탐색 => ";
+	g.resetVisited();
+	g.BFS(0);
+	return 0;
+}
--- cpp_Practice/BaekJoon/Baekjoon14889.cpp
+++ cpp_Practice/BaekJoon/Baekjoon14889.cpp
@@ -1,83 +1,38 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <math.h>
-#include <map>
-using namespace std;
-int GetStats(int x,int y,int arr[20][20]) {
-	if (arr[x][y] == 0 || arr[y][x] == 0) return 0;
-	return arr[x][y] + arr[y][x];
-}
-
-int main(void) {
-	int n;
-	cin >> n;
-
-	int linkArr[20][20];
-	//vector<int> memo;
-
-	map<int, int> memo;
-	int result = 0;
-	
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			int tmp;
-			cin >> tmp;
-			linkArr[j][i] = tmp;
-		}
-	}
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			int compareVal = GetStats(i, j, linkArr);
-			if (compareVal == 0) continue;
-
-			if (memo.find(compareVal) != memo.end()) {
-				memo[compareVal] += 1;
-			}
-			else {
-				memo.insert(std::make_pair(compareVal, 1));
-			}
-		}
-	}
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			int statVal = GetStats(i, j, linkArr);
-			if (statVal == 0) continue;
-
-			int closestVal = 99999;
-			for (auto it = memo.begin(); it != memo.end();) {
-				int compareVal = abs(statVal - it->first);
-
-				if (compareVal >= 0 && compareVal < closestVal) {
-					closestVal = compareVal;
-
-					if (it->second >= 1) {
-						memo[it->first] -= 1;
-					}
-					else {
-						it = memo.erase(it);
-					}
-					//Vector에서 제거하는 것도 실행시켜주어야 함
-					///cout << "ClosestVal: " << closestVal;
-				}
-				else {
-					it++;
-				}
-			}
-			result += closestVal;
-		}
-	}
-	cout << result;
-	
-	
-
-
-
-
-
-
-	
-	return 0;
-}	
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <math.h>
+//#include <map>
+//using namespace std;
+//
+//int main(void) {
+//	int n;
+//	cin >> n;
+//
+//	int linkArr[20][20];
+//	vector<int> memo;
+//
+//	int startSum = 0;
+//	int linkSum = 0;
+//	
+//		
+//	//스타트 팀 링크 팀 두개로 나누어서 두 개를 빼서 절대값으로 바꿔줌
+//
+//	int result = 0;
+//	
+//	for (int i = 0; i < n; i++) {
+//		for (int j = 0; j < n; j++) {
+//			cin >> linkArr[i][j];
+//		}
+//	}
+//
+//	for (int i = 0; i < n; i++) {
+//		for (int j = 0; j < n; j++) {
+//			startSum += linkArr[startTeam[i]][startTeam[j]] + linkArr[startTeam[j]][startTeam[i]];
+//			linkSum += linkArr[linkTeam[i]][linkTeam[j]] + linkArr[linkTeam[j]][linkTeam[i]];
+//		}
+//	}
+//
+//	cout << result;
+//	return 0;
+//}	
