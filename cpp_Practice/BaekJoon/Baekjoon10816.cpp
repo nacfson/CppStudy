6c7abe15532cadb57df6c3ba33649c4831c936d4
nacfson <nacfson@gmail.com>
2023-09-04 08:58:35 +0900

    baekjoon10816

---
+++ cpp_Practice/BaekJoon/Baekjoon10816.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int n, m;
+	map<int, int> nMap;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int tmp;
+		cin >> tmp;
+		if (nMap.find(tmp) == nMap.end()) {
+			nMap.insert({ tmp,1 });
+		}
+		else {
+			nMap[tmp]++;
+		}
+	}
+	cin >> m;
+	for (int i = 0; i < m; i++) {
+		int findVal;
+		cin >> findVal;
+		if (nMap.find(findVal) != nMap.end()) {
+			cout << nMap[findVal] << ' ';
+		}
+		else {
+			cout << '0' << ' ';
+		}
+	}
+	return 0;
+}
