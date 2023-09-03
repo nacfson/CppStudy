1e843e5edfbbe3fd58edfe7783df775099b469f5
Yoo Hyung Joo <65493803+nacfson@users.noreply.github.com>
2023-09-03 09:28:39 +0900

    비행기 타고가요

---
+++ cpp_Practice/BaekJoon/BaekJoon1026.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+bool desc(int a, int b) {
+	return a > b;
+}
+int main(void) {
+	int n;
+	int aArr[100];
+	int bArr[100];
+	int result = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> aArr[i];
+	}
+	for (int i = 0; i < n; i++) {
+		cin >> bArr[i];
+	}
+
+	sort(aArr, aArr + n);
+	sort(bArr, bArr + n,desc);
+
+
+	for (int i = 0; i < n; i++) {
+		result += aArr[i] * bArr[i];
+	}
+	cout << result;
+
+	return 0;
+}
--- cpp_Practice/BaekJoon/Baekjoon1406.cpp
+++ cpp_Practice/BaekJoon/Baekjoon1406.cpp
@@ -1,56 +1,56 @@
-#include <iostream>
-#include <stack>
-#include <string>
-
-using namespace std;
-
-int main() {
-    string initial;
-    cin >> initial;
-
-    stack<char> leftStack, rightStack;
-
-    for (char c : initial) {
-        leftStack.push(c);
-    }
-
-    int numCommands;
-    cin >> numCommands;
-    cin.ignore(); // 개행 문자 제거
-
-    while (numCommands--) {
-        string command;
-        getline(cin, command);
-
-        if (command[0] == 'P') {
-            char c = command[2];
-            leftStack.push(c);
-        }
-        else if (command[0] == 'L' && !leftStack.empty()) {
-            char c = leftStack.top();
-            leftStack.pop();
-            rightStack.push(c);
-        }
-        else if (command[0] == 'D' && !rightStack.empty()) {
-            char c = rightStack.top();
-            rightStack.pop();
-            leftStack.push(c);
-        }
-        else if (command[0] == 'B' && !leftStack.empty()) {
-            leftStack.pop();
-        }
-    }
-
-    // 스택에 저장된 문자열을 출력
-    while (!leftStack.empty()) {
-        rightStack.push(leftStack.top());
-        leftStack.pop();
-    }
-
-    while (!rightStack.empty()) {
-        cout << rightStack.top();
-        rightStack.pop();
-    }
-
-    return 0;
-}
+//#include <iostream>
+//#include <stack>
+//#include <string>
+//
+//using namespace std;
+//
+//int main() {
+//    string initial;
+//    cin >> initial;
+//
+//    stack<char> leftStack, rightStack;
+//
+//    for (char c : initial) {
+//        leftStack.push(c);
+//    }
+//
+//    int numCommands;
+//    cin >> numCommands;
+//    cin.ignore(); // 개행 문자 제거
+//
+//    while (numCommands--) {
+//        string command;
+//        getline(cin, command);
+//
+//        if (command[0] == 'P') {
+//            char c = command[2];
+//            leftStack.push(c);
+//        }
+//        else if (command[0] == 'L' && !leftStack.empty()) {
+//            char c = leftStack.top();
+//            leftStack.pop();
+//            rightStack.push(c);
+//        }
+//        else if (command[0] == 'D' && !rightStack.empty()) {
+//            char c = rightStack.top();
+//            rightStack.pop();
+//            leftStack.push(c);
+//        }
+//        else if (command[0] == 'B' && !leftStack.empty()) {
+//            leftStack.pop();
+//        }
+//    }
+//
+//    // 스택에 저장된 문자열을 출력
+//    while (!leftStack.empty()) {
+//        rightStack.push(leftStack.top());
+//        leftStack.pop();
+//    }
+//
+//    while (!rightStack.empty()) {
+//        cout << rightStack.top();
+//        rightStack.pop();
+//    }
+//
+//    return 0;
+//}
