6e22923e9f73d193d66c311ad8b2d12eacc67cac
Yoo Hyung Joo <65493803+nacfson@users.noreply.github.com>
2023-09-16 11:21:06 +0900

    DFS<BFS

--- cpp_Practice/BaekJoon/Baekjoon1260.cpp
+++ cpp_Practice/BaekJoon/Baekjoon1260.cpp
@@ -1,78 +1,63 @@
 #include <iostream>
-#include <vector>
+#include <set>
 #include <queue>
-
 using namespace std;
+int verCnt, lineCnt;
+set<pair<int, int>> linkedSet;
+bool dfsVisited[1000] = {};
+bool bfsVisited[1000] = {};
 
-vector<vector<int>> mapVec;
-int vertexCnt, lineCnt, startVertex;
-
-vector<int> dfsVec;
-vector<int> bfsVec;
-
-bool dfsVisited[1001] = {};
-bool bfsVisited[1001] = {};
-
-bool isLinked(int a, int b) {
-	if (mapVec[a][b] == 1) {
-		return true;
-	}
-	return false;
+bool isLinked(int ver1,int ver2) {
+	return linkedSet.find({ ver1,ver2 }) != linkedSet.end() || linkedSet.find({ ver2,ver1 }) != linkedSet.end();
 }
 
-void dfs(int vertex) {
-	dfsVec.push_back(vertex);
-	dfsVisited[vertex] = true;
-
-	for (int i = 1; i <= vertexCnt; i++) {
+void dfs(int startVer) {
+	dfsVisited[startVer] = true;
+	cout << startVer << ' ';
+	for (int i = 1; i <= verCnt; i++) {
 		if (dfsVisited[i]) continue;
-		if (isLinked(vertex, i) && i != vertex) {
+
+		if (isLinked(startVer,i)) {
 			dfs(i);
 		}
 	}
+
 }
-
-void bfs(int vertex) {
+void bfs(int startVer) {
 	queue<int> q;
-	bfsVisited[vertex] = true;
-	bfsVec.push_back(vertex);
-	q.push(vertex);
 
+	bfsVisited[startVer] = true;
+	q.push(startVer);
+	cout << startVer << ' ';
 	while (!q.empty()) {
 		int a = q.front();
 		q.pop();
-		for (int i = 1; i <= vertexCnt; i++) {
+		for (int i = 1; i <= verCnt; i++) {
 			if (bfsVisited[i]) continue;
 
-			if (isLinked(a, i) && a != i) {
+			if (isLinked(a, i)) {
+				cout << i << ' ';
 				bfsVisited[i] = true;
 				q.push(i);
-				bfsVec.push_back(i);
 			}
 		}
 	}
 }
+
 int main(void) {
-	cin >> vertexCnt >> lineCnt >> startVertex;
+	int startVer;
 
-	mapVec.resize(1000, vector<int>(1000, -1));
+	cin >> verCnt >> lineCnt >> startVer;
 
-	for (int i = 0; i < vertexCnt; i++) {
+	for (int i = 0; i < lineCnt; i++) {
 		int tmp1, tmp2;
-		cin >> tmp1 >> tmp2;
-		mapVec[tmp1][tmp2] = 1;
+		cin >> tmp1>> tmp2;
+		linkedSet.insert({ tmp1,tmp2 });
 	}
 
-	dfs(startVertex);
-	bfs(startVertex);
-
-	for (int i = 0; i < dfsVec.size(); i++) {
-		cout << dfsVec[i] << ' ';
-	}
-	cout << endl;
-	for (int i = 0; i < bfsVec.size(); i++) {
-		cout << bfsVec[i] << ' ';
-	}
+	dfs(startVer);
+	cout << '\n';
+	bfs(startVer);
 
 	return 0;
 }
