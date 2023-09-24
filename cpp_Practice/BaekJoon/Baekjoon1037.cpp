17fb5ed49c25c7738c8ed01736828e21f6d75fba
nacfson <nacfson@gmail.com>
2023-09-25 08:54:42 +0900

    dd

---
+++ cpp_Practice/BaekJoon/Baekjoon1037.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void) {
+	int n;
+	int minValue = 1000001;
+	int maxValue = 1;
+	vector<int> measureVec;
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int tmp;
+		cin >> tmp;
+		measureVec.push_back(tmp);
+	}
+
+
+	for (int i = 0; i < n; i++) {
+		int vecValue = measureVec[i];
+		if (vecValue < minValue) {
+			minValue = vecValue;
+		}
+		if (vecValue > maxValue) {
+			maxValue = vecValue;
+		}
+	}
+	cout << minValue * maxValue;
+	return 0;
+}
+
---
+++ cpp_Practice/BaekJoon/Baekjoon2661.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int n;
+	cin >> n;
+
+
+
+
+
+
+	return 0;
+}
